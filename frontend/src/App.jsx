ed53000bb10a521d9c8ba011d746c4c8bd9f20e9
acz19 <acz1992@hotmail.co.uk>
2024-07-04 11:31:48 +0200

    Create SearchBar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,21 @@ import { Container, Box } from "@mui/material";
 import Title from "./components/Title";
 import SearchBar from "./components/SearchBar";
 import ContactList from "./components/ContactList";
+import { useState } from "react";
 
 function App() {
+	const [search, setSearch] = useState("");
 	return (
 		<Container>
 			<Box>
 				<Title />
 				<Box>
-					<SearchBar />
+					<SearchBar
+						value={search}
+						onChange={(e) => {
+							setSearch(e.target.value);
+						}}
+					/>
 				</Box>
 
 				<ContactList />
--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,5 +1,26 @@
-const SearchBar = () => {
-	return <div>Search Bar</div>;
+import { TextField, InputAdornment } from "@mui/material";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+
+const SearchBar = ({ value, onChange }) => {
+	return (
+		<TextField
+			type="search"
+			label="Search"
+			placeholder="Enter Name Here"
+			variant="outlined"
+			max="auto"
+			width="100%"
+			InputProps={{
+				startAdornment: (
+					<InputAdornment position="start">
+						<AccountCircle />
+					</InputAdornment>
+				),
+			}}
+			value={value}
+			onChange={onChange}
+		/>
+	);
 };
 
 export default SearchBar;
