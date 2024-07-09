bc3e7e8486dc1159066398fbd81f080715e4a7f0
acz19 <acz1992@hotmail.co.uk>
2024-07-09 11:18:35 +0200

    Import Resolver into Server

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,11 +2,12 @@ const express = require("express");
 require("dotenv").config();
 const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./schema");
+const resolvers = require("./resolvers");
 
 const app = express();
 
 // Pass imported schema to new ApolloServer instance
-const server = new ApolloServer({ typeDefs });
+const server = new ApolloServer({ typeDefs, resolvers });
 const port = 3000;
 
 app.get("/", (req, res) => {
