f5004f7ac3e43b792285bebe5aa6c2736af310c2
acz19 <acz1992@hotmail.co.uk>
2024-07-09 10:50:41 +0200

    Prepare Apollo Server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,12 @@
 const express = require("express");
+require("dotenv").config();
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
+
 const app = express();
+
+// Pass imported schema to new ApolloServer instance
+const server = new ApolloServer({ typeDefs });
 const port = 3000;
 
 app.get("/", (req, res) => {
