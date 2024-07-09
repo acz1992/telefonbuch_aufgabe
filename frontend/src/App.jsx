21e2ddfd04d6685a57d2d37bcecf79a0ec074738
acz19 <acz1992@hotmail.co.uk>
2024-07-09 13:17:15 +0200

    Pass fetched contacts into ContactsGrid component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,6 @@ import Title from "./components/Title";
 import SearchBar from "./components/SearchBar";
 import ContactsGrid from "./components/ContactsGrid";
 import { useState } from "react";
-import { Person } from "@mui/icons-material";
 
 import { useQuery } from "@apollo/client";
 import { SEARCH_PHONEBOOK } from "./graphql/queries";
@@ -12,14 +11,13 @@ import { SEARCH_PHONEBOOK } from "./graphql/queries";
 function App() {
 	const [search, setSearch] = useState("");
 
+	// Executed Query fetches relevant data
 	const { loading, error, data } = useQuery(SEARCH_PHONEBOOK, {
 		variables: { name: search },
 	});
 
-	// Search Functionality Logic
-	const filteredContacts = contacts.filter((contact) =>
-		contact.name.toLowerCase().includes(search.toLowerCase())
-	);
+	// If data exists, data fetched
+	const contacts = data ? data.searchPhonebook : [];
 
 	return (
 		<Container>
@@ -34,7 +32,7 @@ function App() {
 					/>
 				</Box>
 
-				<ContactsGrid contacts={filteredContacts} />
+				<ContactsGrid contacts={contacts} />
 			</Box>
 		</Container>
 	);
