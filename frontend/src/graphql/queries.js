ecaa0d86466f58de27695fd9c7dc076c561a4694
acz19 <acz1992@hotmail.co.uk>
2024-07-10 08:57:07 +0200

    Update GraphQl query to fetch paginated results

--- frontend/src/graphql/queries.js
+++ frontend/src/graphql/queries.js
@@ -1,10 +1,17 @@
 import { gql } from "@apollo/client";
 
 export const SEARCH_PHONEBOOK = gql`
-	query SearchPhonebook($name: String!) {
-		searchPhonebook(name: $name) {
-			name
-			phone
+	query SearchPhonebook($name: String, $pageSize: Int!, $pageIndex: Int!) {
+		searchPhonebook(
+			name: $name
+			pageSize: $pageSize
+			pageIndex: $pageIndex
+		) {
+			totalCount
+			contacts {
+				name
+				phone
+			}
 		}
 	}
 `;
