501432b02594dea19fb870d0d73170c3b9a66a6a
acz19 <acz1992@hotmail.co.uk>
2024-07-10 08:48:32 +0200

    Update Resolver for pagination

--- backend/src/resolvers/index.js
+++ backend/src/resolvers/index.js
@@ -2,10 +2,21 @@ const phoneData = require("../../data/telefonbuch.json");
 
 const resolvers = {
 	Query: {
-		searchPhonebook: (_, { name }) => {
-			return phoneData.filter((entry) =>
-				entry.name.toLowerCase().includes(name.toLowerCase())
-			);
+		searchPhonebook: (_, { name, pageSize, pageIndex }) => {
+			const filteredData = name
+				? phoneData.filter((entry) =>
+						entry.name.toLowerCase().includes(name.toLowerCase())
+				  )
+				: phoneData;
+
+			const start = pageIndex * pageSize;
+			const end = start + pageSize;
+			const paginatedData = filteredData.slice(start, end);
+
+			return {
+				totalCount: filteredData.length,
+				contacts: paginatedData,
+			};
 		},
 	},
 };
