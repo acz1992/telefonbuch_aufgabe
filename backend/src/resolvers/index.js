be7d71eb459f5ba478a3dce9502e3a5ff7874392
acz19 <acz1992@hotmail.co.uk>
2024-07-10 08:49:56 +0200

    Add comments for updated resolver

--- backend/src/resolvers/index.js
+++ backend/src/resolvers/index.js
@@ -3,14 +3,18 @@ const phoneData = require("../../data/telefonbuch.json");
 const resolvers = {
 	Query: {
 		searchPhonebook: (_, { name, pageSize, pageIndex }) => {
+			// Filter data based on search value
 			const filteredData = name
 				? phoneData.filter((entry) =>
 						entry.name.toLowerCase().includes(name.toLowerCase())
 				  )
 				: phoneData;
 
+			// Calculate start and end indices for pagination
 			const start = pageIndex * pageSize;
 			const end = start + pageSize;
+
+			// Slice data to get only relevant page
 			const paginatedData = filteredData.slice(start, end);
 
 			return {
