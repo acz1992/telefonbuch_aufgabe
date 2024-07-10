c7c4f2c1c740111eb5236053e77af757a2d3c2ad
acz19 <acz1992@hotmail.co.uk>
2024-07-10 09:42:37 +0200

    Remove sub-heading

--- frontend/src/components/ContactsGrid.jsx
+++ frontend/src/components/ContactsGrid.jsx
@@ -4,10 +4,6 @@ import ContactCard from "./ContactCard";
 const ContactsGrid = ({ contacts }) => {
 	return (
 		<>
-			<Typography my={2} textAlign="center" variant="h4">
-				Contact List
-			</Typography>
-
 			<Grid container spacing={2}>
 				{contacts.map((contact, index) => (
 					<Grid item xs={12} sm={6} md={4} key={index}>
