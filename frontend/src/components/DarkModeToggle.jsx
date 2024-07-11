95017773961068a3919846d8a5d2fff122ce4a3f
acz19 <acz1992@hotmail.co.uk>
2024-07-11 11:05:10 +0200

    Add bold hover effect for Dark Mode Toggle

--- frontend/src/components/DarkModeToggle.jsx
+++ frontend/src/components/DarkModeToggle.jsx
@@ -1,30 +1,50 @@
 import React from "react";
-import IconButton from "@mui/material/IconButton";
-import { Typography, Box } from "@mui/material";
-import Brightness4Icon from "@mui/icons-material/Brightness4";
-import Brightness7Icon from "@mui/icons-material/Brightness7";
+import { IconButton, Typography, Box } from "@mui/material";
+import {
+	Brightness4 as Brightness4Icon,
+	Brightness7 as Brightness7Icon,
+} from "@mui/icons-material";
 import { useTheme } from "@mui/material/styles";
 import { useDarkMode } from "../hooks/useDarkMode.js";
 
 const DarkModeToggle = () => {
-	const theme = useTheme();
-	const darkMode = useDarkMode();
+	const theme = useTheme(); // Get the current theme (light or dark mode)
+	const darkMode = useDarkMode(); // Get the toggle function for dark mode
 
 	return (
-		<IconButton onClick={darkMode.toggleDarkMode} color="inherit">
+		<IconButton
+			onClick={darkMode.toggleDarkMode} // Toggle between dark and light mode on click
+			color="inherit"
+			sx={{
+				margin: 1,
+				"&:hover": {
+					"& .mode-text": {
+						fontWeight: "bold",
+					},
+				},
+			}}
+		>
 			{theme.palette.mode === "dark" ? (
 				<Box display="flex" alignItems="center">
-					<Typography style={{ marginRight: 8 }} variant="body2">
+					<Typography
+						className="mode-text"
+						style={{ marginRight: 8 }}
+						variant="body2"
+					>
 						Light Mode
-					</Typography>{" "}
-					<Brightness7Icon />{" "}
+					</Typography>
+					<Brightness7Icon />
 				</Box>
 			) : (
 				<Box display="flex" alignItems="center">
-					<Typography style={{ marginRight: 8 }} variant="body2">
+					<Typography
+						className="mode-text"
+						style={{ marginRight: 8 }}
+						variant="body2"
+					>
 						Dark Mode
-					</Typography>{" "}
-					<Brightness4Icon />{" "}
+					</Typography>
+					<Brightness4Icon />
 				</Box>
 			)}
 		</IconButton>
